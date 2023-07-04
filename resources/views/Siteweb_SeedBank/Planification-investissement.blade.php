a142fc3475a9a5731467eebb5788b1f6f25c4b61
Abdoul776 <roducshipping@mail.be>
2023-07-05 00:05:53 +0200

    Ajout partie site

---
+++ resources/views/Siteweb_SeedBank/Planification-investissement.blade.php
@@ -0,0 +1,68 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8" />
+    <title>Seed Finance About us</title>
+    <meta content="width=device-width, initial-scale=1.0" name="viewport" />
+    <meta content="Free HTML Templates" name="keywords" />
+    <meta content="Free HTML Templates" name="description" />
+
+    <!-- Favicon -->
+    <link href="img/favicon.ico" rel="icon" />
+
+    <!-- Google Web Fonts -->
+    <link rel="preconnect" href="https://fonts.gstatic.com" />
+    <link
+      href="https://fonts.googleapis.com/css2?family=Handlee&family=Nunito&display=swap"
+      rel="stylesheet"
+    />
+
+    <!-- Font Awesome -->
+    <link
+      href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.10.0/css/all.min.css"
+      rel="stylesheet"
+    />
+
+    <!-- Flaticon Font -->
+    <link href="lib/flaticon/font/flaticon.css" rel="stylesheet" />
+
+    <!-- Libraries Stylesheet -->
+    <link href="lib/owlcarousel/assets/owl.carousel.min.css" rel="stylesheet" />
+    <link href="lib/lightbox/css/lightbox.min.css" rel="stylesheet" />
+
+    <!-- Customized Bootstrap Stylesheet -->
+    <link href="site_web/css/style.css" rel="stylesheet" />
+  </head>
+
+  <body>
+     <!-- Navbar Start-->
+    @include('Siteweb_SeedBank.nav')
+    <!-- Navbar End -->
+
+    
+    <!-- Header Start -->
+    <div class="container-fluid  px-0 px-md-5 mb-5 bg-images taille">
+        <div class="row align-items-center px-3 ">
+          <div class="col-lg-6 text-center text-lg-left" style="margin-top:100px;">
+           
+            <h3 class="display-3 font-weight-bold text-white">
+             Seed Finance !
+            </h3>
+            
+            <p class="text-white mb-4">
+              Votre banque spécialisée dans les crédits bancaires, assurance, épargne, transferts, investissements en crypto-monnaies... 
+              une banque présente à vos côtés, pensée pour répondre à vos besoins au quotidien.
+             
+            </p>
+            <a href="" class="btn btn-secondary mt-1 py-3 px-4 rounded">En savoir plus </a>
+          </div>
+          <div class="col-lg-6 text-center text-lg-right">
+            <img class="img-fluid mt-5" src="site_web/img/header.png" alt="" />
+          </div>
+        </div>
+      </div>
+      <!-- Header End -->
+  </body>
+
+
+</html>    
